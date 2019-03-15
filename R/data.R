a8fc1c565cb4b8a6fda554156371b4b086892c41
Stefano Coretta <stefanoc.social@me.com>
2019-03-15 09:14:21 +0100

    :memo: document dataframe

--- R/data.R
+++ R/data.R
@@ -5,26 +5,26 @@
 #' @docType data
 #' @format A tibble with 1800 observations and 21 variables.
 #' \describe{
-#'   \item{\code{speaker}}{}
-#'   \item{\code{sentence}}{}
-#'   \item{\code{sentence_ons}}{}
-#'   \item{\code{sentence_off}}{}
-#'   \item{\code{c1_rel}}{}
-#'   \item{\code{c2_rel}}{}
-#'   \item{\code{v1_ons}}{}
-#'   \item{\code{v1_off}}{}
-#'   \item{\code{frame}}{}
-#'   \item{\code{word}}{}
-#'   \item{\code{vowel}}{}
-#'   \item{\code{c2}}{}
-#'   \item{\code{place}}{}
-#'   \item{\code{voicing}}{}
-#'   \item{\code{num_syl}}{}
-#'   \item{\code{sentence_duration}}{}
-#'   \item{\code{rel_rel}}{}
-#'   \item{\code{v1_duration}}{}
-#'   \item{\code{c2_clos_duration}}{}
-#'   \item{\code{speech_rate}}{}
-#'   \item{\code{speech_rate_c}}{}
+#'   \item{\code{speaker}}{The speaker's ID.}
+#'   \item{\code{sentence}}{The sentence stimuli.}
+#'   \item{\code{sentence_ons}}{The onset time of the sentence stimuli (seconds).}
+#'   \item{\code{sentence_off}}{The offset time of the sentence stimuli (seconds).}
+#'   \item{\code{c1_rel}}{The release of C1 (seconds).}
+#'   \item{\code{c2_rel}}{The release of C2 (seconds).}
+#'   \item{\code{v1_ons}}{The onset of V1 (seconds).}
+#'   \item{\code{v1_off}}{THe onset of V2 (seconds).}
+#'   \item{\code{frame}}{The frame sentence.}
+#'   \item{\code{word}}{The target word.}
+#'   \item{\code{vowel}}{The target vowel (V1).}
+#'   \item{\code{c2}}{The second consonant of the target word (C2).}
+#'   \item{\code{place}}{The place of articulation of C2 (velar, labial).}
+#'   \item{\code{voicing}}{The voicing of C2 (voiceless, voiced).}
+#'   \item{\code{num_syl}}{The number of syllables of the target word (disyllabic, monosyllabic).}
+#'   \item{\code{sentence_duration}}{The duration of the sentence stimuli (milliseconds).}
+#'   \item{\code{rel_rel}}{The duration of the releaase-to-release interval (milliseconds).}
+#'   \item{\code{v1_duration}}{The duration of V1 (milliseconds).}
+#'   \item{\code{c2_clos_duration}}{The duration of the closure of C2 (milliseconds).}
+#'   \item{\code{speech_rate}}{The speech rate (number of syllables in the sentence / sentence duration).}
+#'   \item{\code{speech_rate_c}}{Centred speech rate.}
 #' }
 "eng_durations"
